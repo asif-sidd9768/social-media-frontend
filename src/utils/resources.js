0495baaa92b043d58c081ae8d051a0ad8436bc48
asif-sidd9768 <siddiqueasif421@gmail.com>
2023-11-21 15:26:41 +0530

    updated url

--- src/utils/resources.js
+++ src/utils/resources.js
@@ -1,5 +1,4 @@
 export const RESOURCES = {
   // BASE_URL: "http://localhost:3000"
   BASE_URL:"https://techadelic-production.up.railway.app"
-  // BASE_URL:"https://techadelic-backend-production.up.railway.app"
 }
